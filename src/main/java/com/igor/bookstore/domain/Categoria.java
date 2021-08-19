4f475757ee429fa5c640a3b24535d113235aeb85
IgorDmoura <igor.dmoura01@gmail.com>
2021-08-19 19:01:55 -0300

    Finalizando as classes de domínio

--- src/main/java/com/igor/bookstore/domain/Categoria.java
+++ src/main/java/com/igor/bookstore/domain/Categoria.java
@@ -3,20 +3,28 @@ package com.igor.bookstore.domain;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @EqualsAndHashCode
-public class Categoria {
+@Entity
+public class Categoria implements Serializable {
+
+    private static final long seriaVersionUID= 1L;
 
     @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String nome;
 
     private String descricao;
 
+    @OneToMany(mappedBy = "categoria")
     private List<Livro> livros = new ArrayList<>();
 
 }
--- src/main/java/com/igor/bookstore/domain/Livro.java
+++ src/main/java/com/igor/bookstore/domain/Livro.java
@@ -3,11 +3,19 @@ package com.igor.bookstore.domain;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
 @Data
 @EqualsAndHashCode
-public class Livro {
+@Entity
+public class Livro implements Serializable {
+
+    private static final long seriaVersionUID= 1L;
 
     @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String titulo;
@@ -16,5 +24,6 @@ public class Livro {
 
     private String texto;
 
+    @ManyToOne
     private Categoria categoria;
 }
